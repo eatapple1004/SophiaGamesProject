7e607a8e4ca3d364f48faf256f2393a4236c6a65
eatapple1004 <frank.virus0325@gmail.com>
2024-12-24 15:09:33 +0900

    add images & image code

--- phaser_game/src/main.js
+++ phaser_game/src/main.js
@@ -1,27 +1,36 @@
-import Phaser from 'phaser';
-
-// 게임 설정
+// Phaser 게임 설정
 const config = {
-  type: Phaser.AUTO,            // WebGL 또는 Canvas 자동 선택
-  width: 800,                   // 게임 프레임의 너비
-  height: 600,                  // 게임 프레임의 높이
-  backgroundColor: '#e74c3c',   // 배경색
-  parent: 'game-container',     // HTML의 div ID
-  scene: {
-    create,                     // 씬의 생성 함수
-  },
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 800,
+    scene: {
+        preload,
+        create,
+        update,
+    },
 };
+  
+// 리소스 로드
+function preload() {
 
-// Phaser 게임 객체 생성
-const game = new Phaser.Game(config);
+    this.load.image('player', '../assets/images/sample.png');
+    this.load.image('background', '../assets/images/background.jpg');
+    
 
-// create 함수: 게임 화면 초기화
-function create() {
-  // 텍스트 표시
-  this.add.text(300, 250, 'Hello Phaser!', {
-    font: '32px Arial',
-    color: '#ffffff',
-  });
 }
 
-create()
+// 게임 초기화
+function create() {
+
+    const background = this.add.sprite(0, 0, 'background');
+    background.setScale(1);
+    const logo = this.add.image(400, 300, 'player'); // x=400, y=300에 이미지 추가
+    logo.setScale(0.2); // 이미지 크기를 50%로 조정
+}
+
+// 게임 루프
+function update() {}
+
+// Phaser 게임 실행
+new Phaser.Game(config);
+  
